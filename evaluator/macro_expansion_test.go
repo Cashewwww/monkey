3218cf773fd374304f617d3be6a577d20a2ac938
cashew <42924021+cashev@users.noreply.github.com>
2020-04-05 18:55:54 +0900

    p282 test unless

--- evaluator/macro_expansion_test.go
+++ evaluator/macro_expansion_test.go
@@ -90,6 +90,20 @@ func TestExpandMacros(t *testing.T) {
 			`,
 			`(10 - 5) - (2 + 2)`,
 		},
+		{
+			`
+			let unless = macro(condition, consequense, alternative) {
+				quote(if (!(unquote(condition))) {
+					unquote(consequense);
+				} else {
+					unquote(alternative);
+				});
+			}
+
+			unless(10 > 5, puts("not greater"), puts("greater"));
+			`,
+			`if (!(10 > 5)) { puts("not greater") } else { puts("greater") }`,
+		},
 	}
 
 	for _, tt := range tests {
